72a2c74903299f7e2458572469eea4b72ae06b2b
fiefdx <fiefdx@163.com>
2018-03-30 14:10:54 +0800

    add README.md, update test.py

---
+++ README.md
@@ -0,0 +1,17 @@
+# odrspiral
+
+a python package bind the official opendrive spiral example
+
+
+# Install
+
+```bash
+# install
+sudo pip install cython
+sudo python ./setup.py install
+
+# run the test
+cd ./test
+python /test.py
+
+```
--- test/test.py
+++ test/test.py
@@ -11,6 +11,14 @@ if __name__ == "__main__":
     x = 0.0
     y = 0.0
     t = 0.0
+
+    print "#  OpenDRIVE spiral example"
+    print "#  ------------------------\n#"
+    print "#  Computing a spiral with"
+    print "#    initial curvature =   0.000 1/m"
+    print "#    length            = 300.000 m"
+    print "#    curvDot           =   0.001 1/m2\n#"
+
     for s in xrange(0, 300, 1):
         x, y, t = odrspiral.spiral(s, 0.001, x, y, t)
         print "%10.4f %10.4f" % (x, y)
